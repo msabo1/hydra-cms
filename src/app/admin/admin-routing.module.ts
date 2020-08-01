7d99e99164d1432fa8aea91bce4d5116f8fbc727
Mario Sabo <mariosabo2@gmail.com>
2020-08-01 13:37:22 +0200

    Generate roles files

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { UsersComponent } from './users/users.component';
+import { RolesComponent } from './roles/roles.component';
 
 const routes: Routes = [
-    {path: 'users', component: UsersComponent},
-    {path:'', component:AdminHomeComponent}
+  {path:'', component:AdminHomeComponent},
+  {path: 'users', component: UsersComponent},
+  {path:'roles', component:RolesComponent}
 ];
 
 @NgModule({
