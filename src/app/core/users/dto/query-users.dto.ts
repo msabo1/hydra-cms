383c326888cd440b9737688c17813bee4bc66309
Mario Sabo <mariosabo2@gmail.com>
2020-08-15 12:49:06 +0200

    Refactor

--- src/app/core/users/dto/query-users.dto.ts
+++ src/app/core/users/dto/query-users.dto.ts
@@ -9,30 +9,6 @@ export class QueryUsersDto extends QueryDto{
     search?:string;
     roleId?: string;
     status?: string;
-
-    // constructor(getUsersParam?: Partial<GetUsersParam>){
-    //     if(getUsersParam.pageSize){
-    //         this.limit = getUsersParam.pageSize.toString();
-    //     }
-    //     if(getUsersParam.pageIndex){
-    //         this.offset = (getUsersParam.pageIndex * getUsersParam.pageSize).toString();
-    //     }
-    //     if(getUsersParam.sortBy){
-    //         this.sortBy = getUsersParam.sortBy;
-    //     }
-    //     if(getUsersParam.order){
-    //         this.order = getUsersParam.order;
-    //     }
-    //     if(getUsersParam.search){
-    //         this.search = getUsersParam.search;
-    //     }
-    //     if(getUsersParam.roleId){
-    //         this.roleId = getUsersParam.roleId;
-    //     }
-    //     if(getUsersParam.status){
-    //         this.status = getUsersParam.status;
-    //     }
-    // }
 }
 
 export class GetUsersParam extends GetParam{
